3e465c9fdf377d7922c24d11aa6e90a0cb334d31
TianHung <97211743+TianHung@users.noreply.github.com>
2022-11-11 11:10:54 +0700

    success

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
-import './categories_screen.dart';
+
+import './dummy_data.dart';
+import './screens/bottom_tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/filters_screen.dart';
+import './screens/categories_screen.dart';
+import './models/meal.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+ bool _isMealFavorite(String id){
+    return _favoriteMeals.any((meal) => meal.id == id);
+ }
 
   // This widget is the root of your application.
   @override
@@ -30,7 +92,30 @@ class MyApp extends StatelessWidget {
               fontFamily: 'RobotoCondensed',
             )),
       ),
-      home: CategoriesScreen(),
+      //home: CategoriesScreen(),
+      initialRoute: '/',
+      //default is '/'
+      routes: {
+        '/': (ctx) => TabsScreen(_favoriteMeals),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_availableMeals),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(_toggleFavorite, _isMealFavorite),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_setFilters,_filters),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        // if (settings.name == '/meal-detail'){
+        //   return ...;
+        // }else if(settings.name == '/something-else'){
+        //   return ...;
+        // }
+        // return MaterialPageRoute(builder: (ctx)=> CategoriesScreen(),);
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:pasgo/category_item.dart';
-import 'package:pasgo/dummy_data.dart';
-import 'category_item.dart';
+import '../widgets/category_item.dart';
+import '../dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //topApp
-      appBar: AppBar(
-       //titleApp
-        title: Text('DeliMeal'),
-      ),
+    return
       //----------------------AppBody kind: GridView----------------
-      body: GridView(
+        GridView(
         //EdgeInsets make 4 property left, top, right, bottom have the same padding  value.
         padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES//đọc file data
             .map(
-              (catData) => CategoryItem(
-                catData.title,
-                catData.color,
+              (a) => CategoryItem(
+                a.id,
+                a.title,
+                a.color,
               ),
             )
             .toList(),
@@ -30,7 +25,6 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,             //Khoảng cách giữa các item trong hàng ngang
           mainAxisSpacing: 20,              //Khoảng cách giữa các hàng( giữa các item trong cột dọc)
         ),
-      ),
     );
   }
 }
